e83ce2bc8d93f0a304a19418a0f137dbe727eb88
Adil <a_yantizhanov250550@fanshaweonline.ca>
2026-03-02 04:58:30 -0500

    Reset Button Bug Fix.
    Reset button bug fix was implemented with usage of forEach function for the label box and target zones.

--- js/main.js
+++ js/main.js
@@ -3,7 +3,10 @@ console.log("JavaScript File is linked");
 // variables
 const labels = document.querySelectorAll(".label");
 const targetZones = document.querySelectorAll(".target-zone");
+const resetButton = document.querySelector("#reset-btn");
+const labelBox = document.querySelector("#label-box");
 let currrentDraggedElement = null;
+//add variables for resett button and label box
 
 
 // functions
@@ -37,6 +40,17 @@ if(this.children.length>=1) {
     currrentDraggedElement = null;
 }
 
+
+function reset() {
+    labels.forEach(label => {
+        labelBox.appendChild(label);
+    }); // find label and briing back to labelbox
+
+    targetZones.forEach(zone => {
+        zone.innerHTML = "";
+    }); // empty each targetzone
+}
+
 // Event Listeners
 labels.forEach(label => {
     label.addEventListener("dragstart", dragStart);
@@ -46,5 +60,8 @@ targetZones.forEach(zone => {
     zone.addEventListener("dragover", dragOver);
     zone.addEventListener("drop", dropped);
 })
+
+resetButton.addEventListener("click", reset)
+//listen for the click event, call a reset function 
 
 //FIRST CONSOLE THEN DO OTHER THINGS
